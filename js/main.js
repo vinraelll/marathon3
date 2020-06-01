92ef2c2a1b85adceaf6be60bea75ac21a3a41658
Alex Perchun <vinraellll@gmail.com>
2020-06-01 21:48:30 +0300

    release v1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.slider').slick({
+       nextArrow: '<button class="slick-arrow slick-next"><img src="./img/next.svg" alt="next_btn"></button>', 
+       prevArrow: '<button class="slick-arrow slick-prev"><img src="./img/prev.svg" alt="prev_btn"></button>'
+    });
+});
+
+
